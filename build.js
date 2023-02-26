36c3a8cc2f7416083fa723c04865c4161ffff226
Tahasin <110173793+tahasintonmoy2@users.noreply.github.com>
2023-02-26 13:49:51 +0600

    Add files via upload

---
+++ build.js
@@ -0,0 +1,25 @@
+const {MSICreator } =require('electron-wix-msi')
+const path = require('path')
+
+const APP_DIR = path.resolve(__dirname, './ChatGPT-win32-x64')
+const OUT_DIR = path.resolve(__dirname, './windows_installer')
+
+const msiCreator = new MSICreator({
+    appDirectory: APP_DIR,
+    outputDirectory:OUT_DIR,
+
+    description:"ChatGPT by OpenAI",
+    name:"ChatGPT Desktop",
+    exe:"ChatGPT",
+    manufacturer:"Tahasin",
+    version:'1.0.0',
+    icon:"icon.ico",
+
+    ui:{
+        chooseDirectory:true
+    }
+})
+
+msiCreator.create().then(function(){
+    msiCreator.compile()
+})
